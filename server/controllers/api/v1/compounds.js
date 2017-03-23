41af26e3d5f020f9497f1be557b8d2bb1e687031
Gabriel Lerner <gabriel.lerner@avantcredit.com>
2017-03-22 23:57:52 -0500

    full data

--- server/controllers/api/v1/compounds.js
+++ server/controllers/api/v1/compounds.js
@@ -1,22 +1,21 @@
 const { CompoundView, SEARCHABLE } = require('../../../models/compoundView');
 var rdkit = require('../../../util/rdkit');
 
+var compoundToJson = (compound) => ({
+  cid: compound.cid,
+  img: "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/" + compound.cid + "/PNG",
+  props: compound.data.props.map((prop) => ({
+    label: prop.urn.label,
+    name: prop.urn.name,
+    value: prop.value.sval,
+    img: prop.urn.label == 'SMILES' && rdkit.fromSmiles(prop.value.sval),
+  }))
+});
+
 module.exports.show = function(req, res) {
   CompoundView.findOne({
     cid: req.params.cid
-  }).then(compound => {
-    data = {
-      cid: compound.cid,
-      img: "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/" + compound.cid + "/PNG",
-      props: compound.data.props.map((prop) => ({
-        label: prop.urn.label,
-        name: prop.urn.name,
-        value: prop.value.sval,
-        img: prop.urn.label == 'SMILES' && rdkit.fromSmiles(prop.value.sval),
-      }))
-    };
-    res.json(data);
-  });
+  }).then(compound => res.json(compoundToJson(compound)));
 };
 
 module.exports.query = function(req, res) {
@@ -28,6 +27,6 @@ module.exports.query = function(req, res) {
   CompoundView.findAll({
     where: { $or: conditions }
   }).then((compounds) => {
-    res.json(compounds.map((compound) => compound.cid));
+    res.json(compounds.map((compound) => compoundToJson(compound)));
   });
 };
