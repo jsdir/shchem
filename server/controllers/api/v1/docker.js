101c82e3557cc176c7bc4df45c8fc4dcbcd5ef1b
Gabriel Lerner <gabriel.lerner@avantcredit.com>
2017-03-25 13:34:09 -0500

    running idock

--- server/controllers/api/v1/docker.js
+++ server/controllers/api/v1/docker.js
@@ -6,14 +6,18 @@ const cp = require('child_process');
 const { CompoundView, SEARCHABLE } = require('../../../models/compoundView');
 
 const babelSmiToPdbqt = (inFile, outFile) => {
-  cp.execSync(`${path.join(process.env.BABEL_PATH, 'babel')} -ismi ${inFile} -opdbqt ${outFile}`);
+  return cp.execSync(`${path.join(process.env.BABEL_PATH, 'babel')} -ismi ${inFile} -opdbqt ${outFile}`);
+};
+
+const idock = (inputFile, ligandDir, outputDir) => {
+  return cp.execSync(`${path.join(process.env.IDOCK_PATH, 'idock')} --receptor ${inputFile} --ligand ${ligandDir} --out ${outputDir} --center_x 0 --center_y 0 --center_z 0 --size_x 30 --size_y 30 --size_z 30`);
 };
 
 module.exports.dock = function(req, res) {
   CompoundView.findOne({
     cid: req.params.cid
   }).then(compound => {
-    CompoundView.findAll({ limit: 10 }).then(ligands => {
+    CompoundView.findAll({ limit: 3 }).then(ligands => {
       const id = crypto.randomBytes(4).toString("hex");
       const dir = path.join(os.tmpdir(), id);
       fs.mkdirSync(dir);
@@ -23,7 +27,7 @@ module.exports.dock = function(req, res) {
       fs.writeFileSync(inputFile, compound.smiles_isomeric, 'utf-8');
       babelSmiToPdbqt(inputFile, inputFilePdbqt);
 
-      const ligandDir = path.join(dir, 'output');
+      const ligandDir = path.join(dir, 'ligands');
       fs.mkdirSync(ligandDir);
       ligands.forEach(ligand => {
         const ligandFile = path.join(ligandDir, `${ligand.cid}.smi`);
@@ -32,7 +36,9 @@ module.exports.dock = function(req, res) {
         babelSmiToPdbqt(ligandFile, ligandFilePdbqt);
       });
 
-      res.json({ success: true });
+      const outputDir = path.join(dir, 'output');
+      const log = idock(inputFile, ligandDir, outputDir);
+      res.json({ log: log.toString('utf8') });
     });
   });
 };
