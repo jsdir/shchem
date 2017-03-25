ca6249e0b3e4521cabec823498c3a9f177e63af4
Gabriel Lerner <gabriel.lerner@avantcredit.com>
2017-03-25 13:18:04 -0500

    babel convert

--- server/controllers/api/v1/docker.js
+++ server/controllers/api/v1/docker.js
@@ -2,7 +2,12 @@ const os = require("os");
 const fs = require('fs');
 const path = require('path');
 const crypto = require("crypto");
+const cp = require('child_process');
 const { CompoundView, SEARCHABLE } = require('../../../models/compoundView');
+
+const babelSmiToPdbqt = (inFile, outFile) => {
+  cp.execSync(`${path.join(process.env.BABEL_PATH, 'babel')} -ismi ${inFile} -opdbqt ${outFile}`);
+};
 
 module.exports.dock = function(req, res) {
   CompoundView.findOne({
@@ -14,13 +19,17 @@ module.exports.dock = function(req, res) {
       fs.mkdirSync(dir);
 
       const inputFile = path.join(dir, 'input.smi');
+      const inputFilePdbqt = path.join(dir, 'input.pdbqt');
       fs.writeFileSync(inputFile, compound.smiles_isomeric, 'utf-8');
+      babelSmiToPdbqt(inputFile, inputFilePdbqt);
 
       const ligandDir = path.join(dir, 'output');
       fs.mkdirSync(ligandDir);
       ligands.forEach(ligand => {
         const ligandFile = path.join(ligandDir, `${ligand.cid}.smi`);
+        const ligandFilePdbqt = path.join(ligandDir, `${ligand.cid}.pdbqt`);
         fs.writeFileSync(ligandFile, ligand.smiles_isomeric, 'utf-8');
+        babelSmiToPdbqt(ligandFile, ligandFilePdbqt);
       });
 
       res.json({ success: true });
