18674aeacf4de929c6b9b71a758608bc451aae27
Jason Sommer <j@jsdir.net>
2017-03-24 23:35:18 -0500

    Setup travis ci

--- server/test/controllers/compounds_test.js
+++ server/test/controllers/compounds_test.js
@@ -1,11 +1,5 @@
-process.env.NODE_ENV = 'test'
-
 const chai = require('chai')
-const chaiHttp = require('chai-http')
 const server = require('../../server')
-
-chai.should()
-chai.use(chaiHttp)
 
 describe('compounds controller', () => {
   describe('GET', () => {
