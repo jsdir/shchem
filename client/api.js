f2fbab467ee40f2c300b4ef5ccd9891ce2f1f362
Gabriel Lerner <gabriel.lerner@avantcredit.com>
2017-03-26 15:14:40 -0500

    cleanup

--- client/api.js
+++ client/api.js
@@ -4,13 +4,3 @@ export const searchCompounds = query => (
   fetch(`/api/v1/compounds/query?query=${encodeURIComponent(query)}`)
     .then(parseJSON)
 )
-
-export const createJob = data => (
-  fetch('/api/v1/jobs', {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  }).then(parseJSON)
-)
--- server/controllers/api/v1/docking.js
+++ server/controllers/api/v1/docking.js
@@ -1,12 +1,14 @@
 var uuid = require('uuid');
-
 var queue = require('../../../queue');
 
-module.exports.create = function(req, res) {
+module.exports.start = function(req, res) {
   queue.addDockingJob({
     ligandCid: req.body.ligandCid,
     pdbLink: req.body.pdbLink
   }, function(jobId) {
     res.json({ jobId: jobId });
   });
+};
+
+module.exports.showJob = function(req, res) {
 };
