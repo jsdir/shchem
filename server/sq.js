5aa69753b089ac485f71ebd99976069d865219f0
Jason Sommer <j@jsdir.net>
2017-03-26 01:10:42 -0500

    Manually force ssl

--- server/sq.js
+++ server/sq.js
@@ -7,5 +7,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.js')[env];
 
 module.exports = new Sequelize(process.env[config.use_env_variable], {
-  logging: false
+  logging: false,
+  dialectOptions: {
+    ssl: true
+  }
 });
