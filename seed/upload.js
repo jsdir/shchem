b991dd3f097842927f055f5c848acaffbb87caa4
jsdir <jsdir@users.noreply.github.com>
2017-03-27 21:54:59 -0500

    Fix backpressure for upload parsing (#65)

--- seed/upload.js
+++ seed/upload.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const JSONStream = require('JSONStream');
 const BatchStream = require('batch-stream');
+const Writable = require('stream').Writable;
+const inherits = require('util').inherits;
+const process = require('process');
 
 const sq = require('../server/sq');
 const Compound = require('../server/models/compound');
@@ -9,26 +12,36 @@ const mapDataToCompound = require('./mapDataToCompound');
 const readStream = fs.createReadStream(process.argv[2]);
 const stream = JSONStream.parse('PC_Compounds.*');
 
-const batch = new BatchStream({ size: 1000 });
+const batch = new BatchStream({ size: 500 });
 
-var total = 0;
-batch.on('data', function(data) {
+function DatabaseSink() {
+  this.total = 0;
+  Writable.call(this, {
+    objectMode: true,
+    // Only allow a maximum of 3 batches in the stream buffer
+    highWaterMark: 3
+  })
+}
+
+inherits(DatabaseSink, Writable);
+
+DatabaseSink.prototype._write = function(data, _, cb) {
   const query = sq.queryInterface.QueryGenerator.bulkInsertQuery(
     'compounds', data.map(mapDataToCompound)
   ).slice(0, -1) + ' ON CONFLICT DO NOTHING;';
 
-  sq.query(query).then(function() {
-    total += data.length
-    console.log(process.argv[2] + ': Total compounds uploaded: ' + total);
-  }).catch(function(err) {
-    throw err;
-  });
-});
-
-batch.on('error', function(err) {
-  throw err;
-});
+  sq.query(query).then(() => {
+    this.total += data.length
+    console.log(process.argv[2] + ': Total compounds uploaded: ' + this.total);
+    return null;
+  }).then(cb, cb);
+};
 
 readStream
   .pipe(JSONStream.parse('PC_Compounds.*'))
-  .pipe(batch);
+  .pipe(batch)
+  .pipe(new DatabaseSink())
+  .on('error', err => {
+     console.error(err.stack);
+     process.exit(1);
+  });
